775c5a473b2c23a7dfdf7424a7388ef1dd7bdfaa
SergeyEvtush <evtushru208@gmail.com>
2023-05-05 00:06:28 +0200

    start commit

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+const title = "First lesson";
+const screens = 2;
+const screenPrice = 0;
+let rooback;
+const fullPrice = 20;
+const adaptive = true;
+
+alert("first lesson");
+console.log('first lesson');

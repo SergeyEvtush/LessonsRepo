09927f0ecce9b2c757ec9d7f754d2090d2e426a0
SergeyEvtush <evtushru208@gmail.com>
2023-05-06 00:43:56 +0200

    задание по уроку №2

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 "use strict";
-const title = "First lesson";
-const screens = 2;
-const screenPrice = 0;
-let rooback;
-const fullPrice = 20;
+const title = "Second lesson";
+const screens = "Простые, Сложные, Интерактивные";
+const screenPrice = 20;
+const rollback=25;
+const fullPrice = 2000;
 const adaptive = true;
 
-alert("first lesson");
-console.log('first lesson');
+
+console.log(typeof(title),typeof(fullPrice),typeof(adaptive));
+console.log(`длинна строки screens: ${screens.length}`);
+console.log(`стоимость верстки экранов: 
+	${fullPrice} рублей/ 
+	${Math.round(fullPrice / 70)} долларов/
+	${Math.round(fullPrice / 1.5)} гривен/
+	${Math.round(fullPrice / 3)} юаней`);
+console.log(screens.toLowerCase().split(','));
+console.log(`Процент отката посреднику за работу:${fullPrice * (rollback/100)} рублей`);
+

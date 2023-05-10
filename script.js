525fcba1051eda380e4438e8f6ae88a7f92ca347
SergeyEvtush <evtushru208@gmail.com>
2023-05-11 00:07:16 +0200

    задание к уроку 5

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 "use strict";
-let title = prompt('Как называется ваш проект');
-let screens = prompt('Какие типы экранов нужно разработать?');
-let screenPrice = +prompt('Сколько будет стоить данная работа?');
-let adaptive = confirm('Нужен ли адаптив на сайте?');
-let service1 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice1 = +prompt('Сколько это будет стоить?');
-let service2 = prompt('Какой дополнительный тип услуги нужен?');
-let servicePrice2 = +prompt('Сколько это будет стоить?');
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+ let service1;
+let service2;
 const rollback = 10;
-const getAllServicePrices = function (services) {
-	let init = 0;
-	return services.reduce((prev, curr) => prev + curr, 0);
-};
- const allServicePrices = getAllServicePrices([servicePrice1, servicePrice2]);
-const fullPrice = getFullPrice(screenPrice,allServicePrices);
-const servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
 
+const getAllServicePrices = function () {
+	let sum = 0;
+	for(let i=0;i<2;i++)
+	{
+		if (i === 0) {
+			service1 = prompt('Какой дополнительный тип услуги нужен?');
+			
+		} else if (i === 1) { 
+			service2 = prompt('Какой дополнительный тип услуги нужен?');
+		}
+		
+		sum = prompt('Сколько это будет стоить?');
+		while (!isNumber(sum)) {
+			sum += +prompt('Сколько это будет стоить?');
+		};
+	}
+	
+	return sum;
+};
+ 
+
+//проверка на число
+const isNumber = function (num) {
+
+	return !isNaN(parseFloat(num))&&isFinite(num);
+};
+
+const asking = function () {
+	title = prompt('Как называется ваш проект', 'Калькулятор верстки');
+
+	screens = prompt('Какие типы экранов нужно разработать?', 'Простые, сложные');
+
+	
+	do {
+		screenPrice = prompt('Сколько будет стоить данная работа?');
+	} while (!isNumber(screenPrice));
+
+	adaptive = confirm('Нужен ли адаптив на сайте?');
+};
 
 function getServicePercentPrices(fPrice,rBack) { 
 	return Math.ceil(fPrice - (fPrice * (rBack / 100)));
@@ -53,10 +83,15 @@ function getTitle  (titleWord)  {
 	return titleWord.trim().split('').map((item, index) => index === 0 ? item.toUpperCase() : item.toLowerCase()).join('');
 }
 
-
+asking();
+const allServicePrices = getAllServicePrices();
+const fullPrice = getFullPrice(screenPrice,allServicePrices);
+const servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
 showTypeOf(getTitle(title));
 showTypeOf(fullPrice);
 showTypeOf(adaptive);
+console.log('allServicePrices',allServicePrices);
+
 console.log(screens);
 console.log(getRollBackMessage(fullPrice));
 console.log(`стоимость за вычетом процента отката посреднику:${servicePercentPrice} рублей`);
